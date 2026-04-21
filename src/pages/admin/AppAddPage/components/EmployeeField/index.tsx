dcf088faf438e79c2a0163afa1098f2b0ccb12d6
Sergey Bolshakov <spooky5ya@yandex.ru>
2026-04-21 17:34:29 +0300

    SFDM-3605, Добавление сотрудников в приложение

--- src/pages/admin/AppAddPage/components/EmployeeField/index.tsx
+++ src/pages/admin/AppAddPage/components/EmployeeField/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from 'react';
 
+import { Text } from 'components/core';
 import { Autocomplete } from 'components/form';
 import { Button, Icon } from 'components/ui';
 
@@ -12,7 +13,7 @@ export const EmployeeField: FC<IEmployeeField> = ({
     index,
     usersData,
     disabled,
-    append,
+    length,
     remove,
     isOwner,
 }) => {
@@ -23,40 +24,42 @@ export const EmployeeField: FC<IEmployeeField> = ({
         .map((p) => ({
             id: p.id,
             value: p.full_name,
+            email: p.email,
         }));
 
     return (
-        <S.FormRow>
+        <S.FormRow isFirst={index === 0}>
             <S.GrowContainer>
-                <Autocomplete
-                    fullWidth
-                    disabled={disabled}
-                    name={`employees.${index}.employee`}
-                    label="Сотрудник"
-                    options={employeeOptions}
-                    onInputChange={(v) => setSearchText(v)}
-                />
-            </S.GrowContainer>
-            {index === 0 ? (
-                <S.ButtonContainer>
-                    <Button
-                        type="button"
-                        size="medium"
-                        onClick={() => append({ employee: undefined })}
-                        startIcon={<Icon iconName={Icons.Add} />}
+                <S.AutocompleteContainer>
+                    <Autocomplete
+                        fullWidth
+                        disabled={disabled}
+                        name={`employees.${index}.employee`}
+                        label="ФИО"
+                        options={employeeOptions}
+                        onInputChange={(v) => setSearchText(v)}
+                        noOptionsText="Нет совпадений"
+                        makeOption={(option) => (
+                            <div>
+                                <Text variant="body2">{option.value}</Text>
+                                <Text inactive variant="body3">
+                                    {option.email}
+                                </Text>
+                            </div>
+                        )}
                     />
-                </S.ButtonContainer>
-            ) : (
-                <S.ButtonContainer>
+                </S.AutocompleteContainer>
+                {length > 1 && (
                     <Button
-                        type="button"
                         size="medium"
                         disabled={isOwner}
+                        variant="outlined"
+                        type="button"
                         onClick={() => remove(index)}
                         startIcon={<Icon iconName={Icons.Delete} />}
                     />
-                </S.ButtonContainer>
-            )}
+                )}
+            </S.GrowContainer>
         </S.FormRow>
     );
 };
